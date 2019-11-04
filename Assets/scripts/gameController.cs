afe76c7e0c73fdc219f4ee6945cfc3a148622090
natto17 <48194106+natto17@users.noreply.github.com>
2019-11-04 02:59:15 -0500

    Add files via upload

--- Assets/scripts/gameController.cs
+++ Assets/scripts/gameController.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -7,21 +8,42 @@ public class gameController : MonoBehaviour
     public GameObject cubePrefab;
     Vector3 cubePosition;
     public static GameObject activeCube;
-    int airplaneX, airplaneY;
+    int airplaneX, airplaneY, startX, startY, depotX, depotY;
     GameObject[,] cubes;
     int gridWidth, gridHeight;
     bool airplaneActive;
+    float turnLength, turnTimer;
+    int airplaneCap, airplaneCargo;
+    int cargoGain;
+    int score;
     // Start is called before the first frame update
 
     void Start()
     {
-        airplaneX = 0;
-        airplaneY = 8;
-        airplaneActive = false;
-
         gridWidth = 16;
         gridHeight = 9;
         cubes = new GameObject[gridWidth, gridHeight];
+
+        startX = 0;
+        startY = gridHeight-1;
+
+        airplaneX = startX;
+        airplaneY = startY;
+
+        airplaneActive = false;
+
+        turnLength = 1.5f;
+        turnTimer = turnLength;
+
+        airplaneCap = 90;
+        airplaneCargo = 0;
+        cargoGain = 10;
+        score = 0;
+
+        depotX = gridWidth-1;
+        depotY = 0;
+
+
 
         //set cubes in grid
         for (int h = 0; h < gridHeight; h++)
@@ -35,55 +57,90 @@ public class gameController : MonoBehaviour
             }
         }
 
-        //set airplane
+        //set airplane + depot
         cubes[airplaneX, airplaneY].GetComponent<Renderer>().material.color=Color.red;
+        cubes[depotX, depotY].GetComponent<Renderer>().material.color = Color.black;
     }
 
     public void ProcessClick(GameObject cubeClicked, int x, int y)
     {
         // When the player clicks an airplane
 
- 
+
         if (x == airplaneX && y == airplaneY)
         {   //should highlight in some way to show that it’s the active airplane. (Change to a different color, glow, enlarge, etc.)
             //(activate)
             if (!airplaneActive)
             {
                 airplaneActive = true;
-                cubeClicked.transform.localScale *=1.5f;
-            }
-
-            //If the player clicks the active airplane, it should deactivate.
-            //(deactivate)
-            else
+                cubeClicked.transform.localScale *= 1.5f;
+            } else
             {
+
+                //If the player clicks the active airplane, it should deactivate.
+                //(deactivate)
+
                 airplaneActive = false;
                 cubeClicked.transform.localScale /= 1.5f;
             }
-        }
-
-        //If the player clicks clear sky(a white cube) while there is an active airplane, the airplane should teleport to that location(i.e.the cube that was previously red should now be white, and the clicked cube should now be red).
-        //(move cube)
-        else
-            if (airplaneActive)
+        } else
         {
-            //remove from old spot
-            cubes[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.white;
-            cubes[airplaneX, airplaneY].transform.localScale /= 1.5f;
 
-            //move to new spot
-            airplaneX = x;
-            airplaneY = y;
-            cubes[x,y].GetComponent<Renderer>().material.color = Color.red;
-            cubes[x,y].transform.localScale *= 1.5f;
+            //If the player clicks clear sky(a white cube) while there is an active airplane, the airplane should teleport to that location(i.e.the cube that was previously red should now be white, and the clicked cube should now be red).
+            //(move cube)
+
+            if (airplaneActive)
+            {
+                //remove from old spot
+                if (airplaneX == depotX && airplaneY == depotY)
+                {
+                    cubes[depotX, depotY].GetComponent<Renderer>().material.color = Color.black;
+                    cubes[depotX, depotY].transform.localScale /= 1.5f;
+
+                } else
+                {
+                    cubes[airplaneX, airplaneY].GetComponent<Renderer>().material.color = Color.white;
+                    cubes[airplaneX, airplaneY].transform.localScale /= 1.5f;
+                }
+
+                //move to new spot
+                airplaneX = x;
+                airplaneY = y;
+                cubes[x, y].GetComponent<Renderer>().material.color = Color.red;
+                cubes[x, y].transform.localScale *= 1.5f;
+            }
         }
-        
     }
 
+    void LoadCargo()
+    {
+        if (airplaneX == startX && airplaneY == startY)
+        {
+            airplaneCargo += cargoGain;
+                airplaneCargo = Math.Min(airplaneCargo, airplaneCap);
+        }
+    }
+    void DeliverCargo()
+    {
+        if (airplaneX== depotX && airplaneY == depotY)
+        {
+            score += airplaneCargo;
+            airplaneCargo = 0;
+        }
+    }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Time.time > turnTimer)
+        {//take a turn
+            //upper left, give cargo
+            LoadCargo();
+            //lower right, score points
+            DeliverCargo();
+
+            print("Cargo: " + airplaneCargo+ " _ Score: " + score);
+            turnTimer += turnLength;
+        }
     }
 }
